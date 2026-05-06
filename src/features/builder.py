ec5a8aba03c58c176fc3400d2bc2fec85f446369
calabi <alexandra_kozma@mail.ru>
2026-05-06 09:47:28 +0300

    upd feature builder

--- src/features/builder.py
+++ src/features/builder.py
@@ -6,31 +6,34 @@ class FeatureBuilder:
     def __init__(
         self,
         input_columns: list[str],
-        output_column: str = "features",
-        scaled_output_column: str = "scaled_features",
+        features_col: str,
+        scaled_features_col: str,
     ):
         self.input_columns = input_columns
-        self.output_column = output_column
-        self.scaled_output_column = scaled_output_column
+        self.features_col = features_col
+        self.scaled_features_col = scaled_features_col
 
     def assemble_features(self, df: DataFrame) -> DataFrame:
         assembler = VectorAssembler(
             inputCols=self.input_columns,
-            outputCol=self.output_column
+            outputCol=self.features_col,
         )
+
         return assembler.transform(df)
 
     def scale_features(self, df: DataFrame) -> DataFrame:
         scaler = StandardScaler(
-            inputCol=self.output_column,
-            outputCol=self.scaled_output_column,
+            inputCol=self.features_col,
+            outputCol=self.scaled_features_col,
             withStd=True,
             withMean=True,
         )
+
         scaler_model = scaler.fit(df)
         return scaler_model.transform(df)
 
     def build(self, df: DataFrame) -> DataFrame:
         featured_df = self.assemble_features(df)
         featured_df = self.scale_features(featured_df)
+
         return featured_df
